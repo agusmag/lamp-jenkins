faaaf472860d3e3e985c611416f86b0038d83122
Agustin Magliano <agustin-@live.com>
2019-10-14 23:16:02 -0300

    Update test to pass Jenkins test

--- WelcomeTest.php
+++ WelcomeTest.php
@@ -13,7 +13,7 @@
 
 		public function testGreet(){
 			$result = $this->welcome->greet();
-			$this->assertEquals("Welcome to DevOps on AWS", $result);
+			$this->assertEquals("Welcome to DevOps on AWS!", $result);
 		}
 	}
 ?>
